7846d686eea521c8d7a2686b58ef5f3696706091
zeik <alexgermosen@gmail.com>
2025-05-09 09:50:25 -0400

    documentation de tipos

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,8 +1,17 @@
+// Interface con tipo de request para /api/auth/login
 export interface LoginData {
   email: string;
   password: string;
 }
 
+// Interface con tipo responde de /api/auth/login
+export interface LoginResponse {
+  token: string;
+  user: User
+  expiresIn: number;
+}
+
+// Interface con tipo de response de usuario /api
 export interface User {
     _id: string;
     name: string;
@@ -12,10 +21,4 @@ export interface User {
     tel?: string;
     shift?: string;
     registerDate: string;
-}
-
-export interface LoginResponse {
-  token: string;
-  user: User
-  expiresIn: number;
 }
